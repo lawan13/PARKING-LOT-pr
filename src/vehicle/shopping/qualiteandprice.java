2dd8cae9abdc90e5eac8163706d1ffde87375dce
lawan13 <DELL@DELL->
2019-11-27 20:40:03 +0300

    ..................,,,,

---
+++ src/vehicle/shopping/qualiteandprice.java
@@ -0,0 +1,22 @@
+package vehicle.shopping;
+
+public class qualiteandprice extends  parkinlot {
+public int lifeinshornce=100;
+    public String qualite;
+
+    public String getqualite() {
+        return qualite;
+    }
+
+    public void setqualite(String newqualite) {
+        qualite = newqualite;
+    }
+    void inshornce(){
+        int lifeinshornce=67;
+        int safty=99;
+        this.lifeinshornce=lifeinshornce+safty;
+    }
+}
+    
+        
+
